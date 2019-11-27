1c810753e1b7f800c43c4e51acd555f7139b62db
miki <mikiboy004>
2019-11-27 16:41:10 +0700

    menu package

--- application/modules/front-end/views/package.php
+++ application/modules/front-end/views/package.php
@@ -1,132 +1,144 @@
+<br>
+<h2 class="form-group text-center">Package</h2>
 <div class="container">
-                  <div class="row" >
-                    <div class="col-lg-4 col-md-4 col-12 ">
-                            <article class="single_product">
-                                <figure>
-                                    <div class="product_thumb">
-                                      
-                                        <div class="label_product">
-                                            <span class="label_sale">sale</span>
-                                        </div>
-                                     
-                                       
-                                    </div>
-                                    <div class="product_content grid_content">
-                                       <div class="price_box">
-                                          
-                                            <span class="current_price">$79.00</span>  
-                                        </div>
-                                       
-                                        <h3 class="product_name grid_name"><a href="product-details.html">Natus erro at congue massa commodo sit</a></h3>
-                                    </div>
-                                    <div class="product_content list_content">
-                                        <div class="left_caption">
-                                           <div class="price_box">
-                                          
-                                                <span class="current_price">$79.00</span>  
-                                            </div>
-                                            <h3 class="product_name"><a href="product-details.html">Natus erro at congue massa commodo sit</a></h3>
-                                         
-                                            <div class="product_desc">
-                                                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco,Proin lectus ipsum, gravida et mattis vulputate, tristique ut lectus</p>
-                                            </div>
-                                        </div>
-                                        <div class="right_caption">
-                                            <div class="add_to_cart">
-                                                <a href="cart.html" title="add to cart">Add to cart</a>
-                                            </div>
-                                            <div class="action_links">
-                                                <ul>
-                                                    <li class="wishlist"><a href="wishlist.html" title="Add to Wishlist"><i class="fa fa-heart-o" aria-hidden="true"></i>  Add to Wishlist</a></li>
-                                                    <li class="compare"><a href="#" title="compare"><span class="ion-levels"></span> Compare</a></li>
-                                                    <li class="quick_button"><a href="#" data-toggle="modal" data-target="#modal_box"  title="quick view"> <span class="ion-ios-search-strong"></span> Quick View</a></li>
-                                                </ul>
-                                            </div>
-                                        </div>
-                                    </div>
-                                </figure>
-                            </article>
+    <div class="row">
+
+        <div class="col-lg-4 col-md-4 col-12 ">
+            <article class="single_product" style="border-radius:5%; box-shadow: 0 5px 50px 0 rgba(0, 0, 0, 0.2);">
+            <div class="label_product">
+                    <span class="label_sale">sale</span>
+            </div>
+                <figure>
+                    <div class="product_thumb">
+
+                     
+
+
+                    </div>
+                    <div class="product_content grid_content">
+                        <h4 style="MARGIN-BOTTOM: 3rem;">Annual</h4>
+                        <div class="price_box">
+
+                            <span class="current_price" style="font-size:25px">$9.95/mo</span>
                         </div>
-                        <div class="col-lg-4 col-md-4 col-12 ">
-                            <article class="single_product">
-                                <figure>
-                                    <div class="product_thumb">
-                                      
-                                        <div class="label_product">
-                                            <span class="label_sale">sale</span>
-                                        </div>
-                                     
-                                    </div>
-                                  
-                                    <div class="product_content grid_content">
-                                    <h4>Annual</h4>
-                                       <div class="price_box">
-                                           
-                                            <span class="current_price">$79.00/mo</span>  
-                                        </div>
-                                        
-                                        <h3 class="product_name grid_name"><a href="product-details.html">Natus erro at congue massa commodo sit</a></h3>
-                                    </div>
-                                    <div class="product_content list_content">
-                                        <div class="left_caption">
-                                           <div class="price_box">
-                                              
-                                                <span class="current_price">$79.00</span>  
-                                            </div>
-                                            <h3 class="product_name"><a href="product-details.html">Natus erro at congue massa commodo sit</a></h3>
-                                           
-                                            <div class="product_desc">
-                                                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco,Proin lectus ipsum, gravida et mattis vulputate, tristique ut lectus</p>
-                                            </div>
-                                        </div>
-                                        <div class="right_caption">
-                                            <div class="add_to_cart">
-                                                <a href="cart.html" title="add to cart">Add to cart</a>
-                                            </div>
-                                          
-                                        </div>
-                                    </div>
-                                </figure>
-                            </article>
+                    <h3 class="product_name grid_name"><a href="product-details.html">Natus erro at congue massa commodo sit</a></h3>
+                    <h3 class="product_name">Natus erro at congue massa commodo sit</a></h3><br>
+
+                        <button type="button" class="btn btn-primary" style="width: 80%">Choose Annual</button>
+                    </div>
+                    <div class="product_content list_content">
+                        <div class="left_caption">
+                            <h4 style="MARGIN-BOTTOM: 3rem;">Annual</h4>
+                            <div class="price_box">
+
+                                <span class="current_price" style="font-size:25px">$9.95/mo</span>
+                            </div>
+                            <h3 class="product_name"><a href="product-details.html">Natus erro at congue massa commodo sit</a></h3>
+                            <h3 class="product_name">Natus erro at congue massa commodo sit</a></h3><br>
+
+                            <div class="product_desc">
+                                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco,Proin lectus ipsum, gravida et mattis vulputate, tristique ut lectus</p><br>
+                            </div>
+                            <button type="button" class="btn btn-primary" style="width: 80%">Choose Annual</button>
                         </div>
-                        <div class="col-lg-4 col-md-4 col-12 ">
-                            <article class="single_product">
-                                <figure>
-                                    <div class="product_thumb">
-                                      
-                                        <div class="label_product">
-                                            <span class="label_sale">sale</span>
-                                        </div>
-                                     
-                                    
-                                    </div>
-                                    <div class="product_content grid_content">
-                                       <div class="price_box">
-                                          
-                                            <span class="current_price">$79.00</span>  
-                                        </div>
-                                        
-                                        <h3 class="product_name grid_name"><a href="product-details.html">Natus erro at congue massa commodo sit</a></h3>
-                                    </div>
-                                    <div class="product_content list_content">
-                                        <div class="left_caption">
-                                           <div class="price_box">
-                                               
-                                                <span class="current_price">$79.00</span>  
-                                            </div>
-                                            <h3 class="product_name"><a href="product-details.html">Natus erro at congue massa commodo sit</a></h3>
-                                           
-                                            <div class="product_desc">
-                                                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco,Proin lectus ipsum, gravida et mattis vulputate, tristique ut lectus</p>
-                                            </div>
-                                        </div>
-                                        <div class="right_caption">
-                                         
-                                        </div>
-                                    </div>
-                                </figure>
-                            </article>
+                        <div class="right_caption">
+
+                            <div class="action_links">
+                                <ul>
+                                    <li class="wishlist"><a href="wishlist.html" title="Add to Wishlist"><i class="fa fa-heart-o" aria-hidden="true"></i> Add to Wishlist</a></li>
+                                    <li class="compare"><a href="#" title="compare"><span class="ion-levels"></span> Compare</a></li>
+                                    <li class="quick_button"><a href="#" data-toggle="modal" data-target="#modal_box" title="quick view"> <span class="ion-ios-search-strong"></span> Quick View</a></li>
+                                </ul>
+                            </div>
                         </div>
+                    </div>
+                </figure>
+            </article>
+        </div>
+        <div class="col-lg-4 col-md-4 col-12 ">
+            <article class="single_product" style="border-radius:5%;box-shadow: 0 5px 50px 0 rgba(0, 0, 0, 0.2);">
+                <figure>
+                    <div class="product_thumb">
+
+                       
+
+                    </div>
+
+                    <div class="product_content grid_content">
+                        <h4 style="MARGIN-BOTTOM: 3rem;">Quarterly</h4>
+                        <div class="price_box">
+
+                            <span class="current_price" style="font-size:25px">$19.95/mo</span>
                         </div>
+
+                        <h3 class="product_name grid_name"><a href="product-details.html">Natus erro at congue massa commodo sit</a></h3>
+                        <h3 class="product_name">Natus erro at congue massa commodo sit</a></h3><br>
+                        <button type="button" class="btn btn-primary" style="width: 80%">Choose Quarterly</button>
+                    </div>
+                    <div class="product_content list_content">
+                        <div class="left_caption">
+                            <h4 style="MARGIN-BOTTOM: 3rem;">Quarterly</h4>
+                            <div class="price_box">
+
+                                <span class="current_price" style="font-size:25px">$19.95/mo</span>
+                            </div>
+                            <h3 class="product_name"><a href="product-details.html">Natus erro at congue massa commodo sit</a></h3>
+                            <h3 class="product_name">Natus erro at congue massa commodo sit</a></h3><br>
+
+                            <div class="product_desc">
+                                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco,Proin lectus ipsum, gravida et mattis vulputate, tristique ut lectus</p>
+                            </div>
+                            <button type="button" class="btn btn-primary" style="width: 80%">Choose Quarterly</button>
                         </div>
-                        
+
+                    </div>
+                </figure>
+            </article>
+        </div>
+        <div class="col-lg-4 col-md-4 col-12 ">
+            <article class="single_product" style="border-radius:5%; box-shadow: 0 5px 50px 0 rgba(0, 0, 0, 0.2);">
+                <figure>
+                    <div class="product_thumb">
+
+                   
+
+
+                    </div>
+                    <div class="product_content grid_content">
+                        <h4 style="MARGIN-BOTTOM: 3rem;">Monthly</h4>
+                        <div class="price_box">
+
+                            <span class="current_price" style="font-size:25px">$39.95/mo</span>
+                        </div>
+
+                        <h3 class="product_name grid_name"><a href="product-details.html">Natus erro at congue massa commodo sit</a></h3>
+                        <h3 class="product_name">Natus erro at congue massa commodo sit</a></h3><br>
+                        <button type="button" class="btn btn-primary" style="width: 80%">Choose Quarterly</button>
+                    </div>
+                    <div class="product_content list_content">
+                        <div class="left_caption">
+                            <h4 style="MARGIN-BOTTOM: 3rem;">Monthly</h4>
+                            <div class="price_box">
+
+                                <span class="current_price" style="font-size:25px">$39.95/mo</span>
+                            </div>
+                            <h3 class="product_name"><a href="product-details.html">Natus erro at congue massa commodo sit</a></h3>
+                            <h3 class="product_name">Natus erro at congue massa commodo sit</a></h3><br>
+                            <button type="button" class="btn btn-primary" style="width: 80%">Choose Quarterly</button>
+                            <div class="product_desc">
+                                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco,Proin lectus ipsum, gravida et mattis vulputate, tristique ut lectus</p><br>
+                            </div>
+                            <button type="button" class="btn btn-primary" style="width: 80%">Choose Quarterly</button>
+                        </div>
+                        <div class="right_caption">
+
+                        </div>
+                    </div>
+                </figure>
+            </article>
+
+        </div>
+
+    </div>
+</div>
+<br>
