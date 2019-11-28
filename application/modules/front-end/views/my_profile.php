57ba3f99eeaaaf93dfa9206fb7974621d3f0f71d
BossPSR <boss3075030750@gmail.com>
2019-11-28 12:57:20 +0700

    fix profile

--- application/modules/front-end/views/my_profile.php
+++ application/modules/front-end/views/my_profile.php
@@ -13,11 +13,14 @@
                      <div class="image_profile">
                         <h2 class="text-center" style="font-size:18px;">My income<br><?php echo number_format("487515"); ?></h2>
                         <div class="image_PF">
-                            <img src="public/frontend/assets/img/3297.png" alt="">
+                            <div class="imageProfile_show">
+                                <img src="public/frontend/assets/img/3297.png" alt="">
+                                <div class="edit_name_user"><a href="#"><i class="fa fa-edit"></i>  Edited</a></div>
+                            </div>
                         </div>
                         <div class="menu_in_image">
-                            <div class="menu_in_image_left"><i class="fa fa-user"></i>  Name : User001</div>
-                            <div class="menu_in_image_right"><a href="#"><i class="fa fa-edit"></i>  Edited</a></div>
+                            <div class="name_user"><i class="fa fa-user"></i>  Name : User001</div>
+                            
                         </div>
                      </div>
 
@@ -25,17 +28,31 @@
 
                      <div class="single_banner menu_profileList">
                        
-                        <div class="result_list_menu">Follower</div>
-                        <div class="result_list_menu">View</div>
-                        <div class="result_list_menu">Unlocked</div>
-                        <div class="result_list_menu">Upload</div>
-                        <div class="result_list_menu">Rejected</div>
+                        <div class="menu_profileRow">
+                            <div class="result_list_menu">
+                                <div class="result_menu"><?php echo number_format("145201"); ?></div>
+                                <div class="list_menu">Follower</div>
+                            </div>
+                            <div class="result_list_menu">
+                                <div class="result_menu"><?php echo number_format("7421985"); ?></div>
+                                <div class="list_menu">View</div>
+                            </div>
+                            <div class="result_list_menu">
+                                <div class="result_menu"><?php echo number_format("1527"); ?></div>
+                                <div class="list_menu">Unlocked</div>
+                            </div>
+                        </div>
 
-                        <div class="result_menu result_menu_1"><?php echo number_format("145201"); ?></div>
-                        <div class="result_menu result_menu_2"><?php echo number_format("7421985"); ?></div>
-                        <div class="result_menu result_menu_3"><?php echo number_format("1527"); ?></div>
-                        <div class="result_menu result_menu_4"><?php echo number_format("150"); ?></div>
-                        <div class="result_menu result_menu_5"><?php echo number_format("47"); ?></div>
+                        <div class="menu_profileRow">
+                            <div class="result_list_menu">
+                                <div class="result_menu"><?php echo number_format("150"); ?></div>
+                                <div class="list_menu">Upload</div>
+                            </div>
+                            <div class="result_list_menu">
+                                <div class="result_menu"><?php echo number_format("47"); ?></div>
+                                <div class="list_menu">Rejected</div>
+                            </div>
+                        </div>
 
                     </div>
 
