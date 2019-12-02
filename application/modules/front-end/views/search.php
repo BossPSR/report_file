fac48be2781636703b66ad0ee76f26d981032f66
Nattaphon Kiattikul <jame0925623256@gmail.com>
2019-12-02 15:02:55 +0700

    Search

--- application/modules/front-end/views/search.php
+++ application/modules/front-end/views/search.php
@@ -8,32 +8,37 @@
         <div class="col-lg-12 col-md-12 col-12 ">
             <article class="single_upload input-upload">
                 <div class="row">
-                    <div class="col-3">
-                        <!-- <iframe src="public/image/ExportServlet.pdf" width="100%" height="550px"></iframe> -->
-                        <object data="public/image/ExportServlet.pdf" type="application/pdf" width="100%" height="620px">
-                            <p>Alternative text - include a link <a href="myfile.pdf">to the PDF!</a></p>
-                        </object>
-                    </div>
-                    <div class="col-3">
+                    <div class="col-3 text-center margin-20">
+                        <label>How to disable downloading</label>
                         <iframe src="public/image/ExportServlet.pdf" width="100%" height="620px"></iframe>
                     </div>
-                    <div class="col-3">
+                    <div class="col-3 text-center margin-20">
+                        <label>How to disable downloading</label>
                         <iframe src="public/image/ExportServlet.pdf" width="100%" height="620px"></iframe>
                     </div>
-                    <div class="col-3">
+                    <div class="col-3 text-center margin-20">
+                        <label>How to disable downloading</label>
+                        <iframe src="public/image/ExportServlet.pdf" width="100%" height="620px"></iframe>
+                    </div>
+                    <div class="col-3 text-center margin-20">
+                        <label>How to disable downloading</label>
                         <iframe src="public/image/ExportServlet.pdf" width="100%" height="620px"></iframe>
                     </div>
                     
-                    <div class="col-3">
+                    <div class="col-3 text-center margin-20">
+                        <label>How to disable downloading</label>
                         <iframe src="public/image/ExportServlet.pdf" width="100%" height="620px"></iframe>
                     </div>
-                    <div class="col-3">
+                    <div class="col-3 text-center margin-20">
+                        <label>How to disable downloading</label>
                         <iframe src="public/image/ExportServlet.pdf" width="100%" height="620px"></iframe>
                     </div>
-                    <div class="col-3">
+                    <div class="col-3 text-center margin-20">
+                        <label>How to disable downloading</label>
                         <iframe src="public/image/ExportServlet.pdf" width="100%" height="620px"></iframe>
                     </div>
-                    <div class="col-3">
+                    <div class="col-3 text-center margin-20">
+                        <label>How to disable downloading</label>
                         <iframe src="public/image/ExportServlet.pdf" width="100%" height="620px"></iframe>
                     </div>
                 </div>
