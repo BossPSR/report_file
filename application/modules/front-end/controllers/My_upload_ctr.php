6c148801f9d5905bf66d6530d154b24e8ecd981a
Nattaphon Kiattikul <jame0925623256@gmail.com>
2019-12-23 19:06:25 +0700

    ลบ My_upload

--- application/modules/front-end/controllers/My_upload_ctr.php
+++ application/modules/front-end/controllers/My_upload_ctr.php
@@ -6,16 +6,16 @@ class My_upload_ctr extends CI_Controller
 
 	public function __construct()
 	{
-        parent::__construct();
-        $this->load->model('Upload_model');
-    }
-    
+		parent::__construct();
+		$this->load->model('Upload_model');
+	}
+
 	function my_upload()
 	{
 		if ($this->session->userdata('email') == '') {
 			redirect('home');
 		} else {
-            $users              = $this->db->get_where('tbl_user',['email' => $this->session->userdata('email')])->row_array();
+			$users              = $this->db->get_where('tbl_user', ['email' => $this->session->userdata('email')])->row_array();
 			$usersid            = $users['id'];
 			$insert_id          = $this->input->get('insert_id');
 			$data['documents']   = $this->Upload_model->my_upload($usersid);
@@ -28,8 +28,15 @@ class My_upload_ctr extends CI_Controller
 			}
 
 			$this->load->view('options/header_login');
-			$this->load->view('my_upload',$data);
+			$this->load->view('my_upload', $data);
 			$this->load->view('options/footer');
 		}
+	}
+
+	public function delete_file()
+	{
+		$id = $this->input->get('Id');
+		$this->db->delete('tbl_upload_preview', array('id' => $id));
+		redirect('my-upload');
 	}
 }
--- application/modules/front-end/views/my_upload.php
+++ application/modules/front-end/views/my_upload.php
@@ -2,49 +2,51 @@
 <h2 class="text-center" style="margin-top: 15px;">My-Upload</h2>
 <hr class="line_package">
 <br>
-<?php if(!empty($documents)) :  ?>
-<div class="container">
-    <div class="row" style="margin-bottom: 100px;">
-        <div class="col-lg-12 col-md-12 col-12 ">
-            <article class="single_upload input-upload">
-                <div class="row">
-                <?php foreach ($documents as $key => $documents) : ?>
-                    <div class="col-3 text-right margin-5 product">
-                        <div class="boxdel">
-                            <button type="button" class="btn btn-danger" class="boxdel"><i class="fa fa-trash-o" aria-hidden="true" style="font-size: 26px;"></i></button>
-                        </div>
-                        <iframe src="<?php echo $documents['path']; ?>" width="100%" height="440px"></iframe>
-                        <div style="color: #a7a7a7; display:flex;justify-content:space-around;">
-                            <span ><i class="fa fa-clock-o" aria-hidden="true"> 20/11/2562</i></span>
-                            <span><i class="fa fa-unlock-alt" aria-hidden="true"> 5,000</i></span>
-                            <span><i class="fa fa-eye" aria-hidden="true"> 1,501</i></span>
-                        </div>
+<?php if (!empty($documents)) :  ?>
+    <div class="container">
+        <div class="row" style="margin-bottom: 100px;">
+            <div class="col-lg-12 col-md-12 col-12 ">
+                <article class="single_upload input-upload">
+                    <div class="row">
+                        <?php foreach ($documents as $key => $documents) : ?>
+                            <div class="col-3 text-right margin-5 product">
+                                <div class="boxdel">
+                                    <a href="#" class="btn btn-info" class="boxdel"><i class="fa fa-file-text" aria-hidden="true" style="font-size: 26px;"></i></a>
+                                    <a href="#" class="btn btn-warning" class="boxdel"><i class="fa fa-pencil" aria-hidden="true" style="font-size: 26px;"></i></a>
+                                    <a onclick="confirmalert2('<?php echo $documents['id']; ?>')" class="btn btn-danger" class="boxdel delete-s remove"><i class="fa fa-trash-o" aria-hidden="true" style="font-size: 26px;"></i></a>
+                                </div>
+                                <iframe src="<?php echo $documents['path']; ?>" width="100%" height="440px"></iframe>
+                                <div style="color: #a7a7a7; display:flex;justify-content:space-around;">
+                                    <span><i class="fa fa-clock-o" aria-hidden="true"> 20/11/2562</i></span>
+                                    <span><i class="fa fa-unlock-alt" aria-hidden="true"> 5,000</i></span>
+                                    <span><i class="fa fa-eye" aria-hidden="true"> 1,501</i></span>
+                                </div>
+                            </div>
+                        <?php endforeach; ?>
                     </div>
-                <?php endforeach ; ?>
-                </div>
-            </article>
+                </article>
+            </div>
         </div>
     </div>
-</div>
-<br>
+    <br>
 
 <?php else : ?>
 
-<!--error section area start-->
-<div class="error_section">
-   <div class="container">   
-       <div class="row">
-           <div class="col-12">
-               <div class="error_form">
-                   <h1>No file</h1>
-                   <h2>Data Not Found</h2>
-                   <p>Sorry, the file you are looking for does not exist.</p>
-                   <a href="upload">Back to home page</a>
-               </div>
-           </div>
-       </div>
-   </div>    
-</div>
-<!--error section area end--> 
+    <!--error section area start-->
+    <div class="error_section">
+        <div class="container">
+            <div class="row">
+                <div class="col-12">
+                    <div class="error_form">
+                        <h1>No file</h1>
+                        <h2>Data Not Found</h2>
+                        <p>Sorry, the file you are looking for does not exist.</p>
+                        <a href="upload">Back to home page</a>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+    <!--error section area end-->
 
 <?php endif; ?>
