1c2f70c71d8d4eab2f45440a08d312d2e9559a38
Nattaphon Kiattikul <jame0925623256@gmail.com>
2019-12-25 10:23:56 +0700

    db

--- report_file.sql
+++ report_file.sql
@@ -1,42 +1,48 @@
-/*
-Navicat MySQL Data Transfer
+-- phpMyAdmin SQL Dump
+-- version 4.9.0.1
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Generation Time: Dec 25, 2019 at 04:23 AM
+-- Server version: 10.4.6-MariaDB
+-- PHP Version: 7.2.22
 
-Source Server         : db
-Source Server Version : 50505
-Source Host           : localhost:3306
-Source Database       : report_file
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET AUTOCOMMIT = 0;
+START TRANSACTION;
+SET time_zone = "+00:00";
 
-Target Server Type    : MYSQL
-Target Server Version : 50505
-File Encoding         : 65001
 
-Date: 2019-12-23 19:11:27
-*/
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
 
-SET FOREIGN_KEY_CHECKS=0;
+--
+-- Database: `report_file`
+--
 
--- ----------------------------
--- Table structure for `tbl_follow`
--- ----------------------------
-DROP TABLE IF EXISTS `tbl_follow`;
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `tbl_follow`
+--
+
 CREATE TABLE `tbl_follow` (
-  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `id` int(11) NOT NULL,
   `sender_userId` int(11) DEFAULT NULL,
   `receiver_userId` int(11) DEFAULT NULL,
-  `create_at` datetime DEFAULT NULL,
-  PRIMARY KEY (`id`)
+  `create_at` datetime DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
--- ----------------------------
--- Records of tbl_follow
--- ----------------------------
+-- --------------------------------------------------------
 
--- ----------------------------
--- Table structure for `tbl_paypal`
--- ----------------------------
-DROP TABLE IF EXISTS `tbl_paypal`;
+--
+-- Table structure for table `tbl_paypal`
+--
+
 CREATE TABLE `tbl_paypal` (
-  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `id` int(11) NOT NULL,
   `orderID` varchar(255) NOT NULL,
   `payerID` varchar(255) NOT NULL,
   `amount` int(11) NOT NULL,
@@ -44,93 +50,186 @@ CREATE TABLE `tbl_paypal` (
   `user_id` int(11) NOT NULL,
   `first_name` varchar(150) NOT NULL,
   `last_name` varchar(150) NOT NULL,
-  `create_time` datetime NOT NULL,
-  PRIMARY KEY (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8;
+  `create_time` datetime NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
--- ----------------------------
--- Records of tbl_paypal
--- ----------------------------
-INSERT INTO `tbl_paypal` VALUES ('1', '5XG65837WK7507415', 'J3GJEC9GX23VS', '1', 'USD', '3', 'John', 'Doe', '2019-10-01 11:50:00');
+--
+-- Dumping data for table `tbl_paypal`
+--
 
--- ----------------------------
--- Table structure for `tbl_upload`
--- ----------------------------
-DROP TABLE IF EXISTS `tbl_upload`;
+INSERT INTO `tbl_paypal` (`id`, `orderID`, `payerID`, `amount`, `currency_code`, `user_id`, `first_name`, `last_name`, `create_time`) VALUES
+(1, '5XG65837WK7507415', 'J3GJEC9GX23VS', 1, 'USD', 3, 'John', 'Doe', '2019-10-01 11:50:00');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `tbl_upload`
+--
+
 CREATE TABLE `tbl_upload` (
-  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `id` int(11) NOT NULL,
   `userId` int(11) NOT NULL,
   `search_item` varchar(255) DEFAULT '',
   `select_item` varchar(255) DEFAULT '',
   `code` text DEFAULT NULL,
   `topic` text DEFAULT NULL,
   `create_at` datetime DEFAULT NULL,
-  `update_at` datetime DEFAULT NULL,
-  PRIMARY KEY (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=6 DEFAULT CHARSET=utf8;
+  `update_at` datetime DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
--- ----------------------------
--- Records of tbl_upload
--- ----------------------------
-INSERT INTO `tbl_upload` VALUES ('5', '2', 'Google เอกสาร สร้างและแก้ไขเอกสารออนไลน์ฟรี', 'Accessories', 'MX0001', 'สร้างเอกสารใหม่และทำการแก้ไขพร้อมๆ กับผู้อื่นจากคอมพิวเตอร์ โทรศัพท์ หรือแท็บเล็ตของคุณ มีหรือไม่มีอินเทอร์เน็ตก็ทำงานเสร็จได้เหมือนกัน ใช้เอกสารเพื่อแก้ไขไฟล์ Word ', '2019-12-23 19:09:18', null);
+--
+-- Dumping data for table `tbl_upload`
+--
 
--- ----------------------------
--- Table structure for `tbl_upload_full`
--- ----------------------------
-DROP TABLE IF EXISTS `tbl_upload_full`;
+INSERT INTO `tbl_upload` (`id`, `userId`, `search_item`, `select_item`, `code`, `topic`, `create_at`, `update_at`) VALUES
+(1, 2, 'ปัญญาประดิษฐ์ (AI : Artificial Intelligence) คืออะไร ???', 'Electronic', 'MX001', 'เครื่องจักร(machine) ที่มีฟังก์ชันทีมีความสามารถในการทำความเข้าใจ เรียนรู้องค์ความรู้ต่างๆ อาทิเช่น การรับรู้  การเรียนรู้ การให้เหตุผล และการแก้ปัญหาต่างๆ  เครื่องจักรที่มีความสามารถเหล่านี้ก็ถือว่าเป็น ปัญญาประดิษฐ์  (AI : Artificial Intelligence) นั่นเอง\r\n\r\nเพราะฉะนั้นจึงสามารถกล่าวได้ว่า AI ถือกำเนิดขึ้นเมื่อเครื่องจักรมีความสามารถที่จะเรียนรู้นั่นเอง  ซึ่ง AI ก็ถูกแบ่งออกเป็นหลายระดับตามความสามารถหรือความฉลาด  โดยจะวัดจากความสามารถในการ ให้เหตุผล การพูด และทัศนคติของ AI ตัวนั้นๆ เมื่อเปรียบเทียบกับมนุษย์อย่างเราๆ', '2019-12-24 10:21:54', NULL);
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `tbl_upload_full`
+--
+
 CREATE TABLE `tbl_upload_full` (
-  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `id` int(11) NOT NULL,
   `userId` int(11) NOT NULL,
   `upload_id` varchar(110) DEFAULT NULL,
   `file_name` varchar(255) DEFAULT NULL,
   `create_at` datetime DEFAULT NULL,
   `update_at` datetime DEFAULT NULL,
-  `path` varchar(255) DEFAULT NULL,
-  PRIMARY KEY (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8;
+  `path` varchar(255) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
--- ----------------------------
--- Records of tbl_upload_full
--- ----------------------------
-INSERT INTO `tbl_upload_full` VALUES ('1', '2', '5', 'sample-3pp.pdf', '2019-12-23 19:11:07', null, 'uploads/Preview/sample-3pp.pdf');
+-- --------------------------------------------------------
 
--- ----------------------------
--- Table structure for `tbl_upload_preview`
--- ----------------------------
-DROP TABLE IF EXISTS `tbl_upload_preview`;
+--
+-- Table structure for table `tbl_upload_preview`
+--
+
 CREATE TABLE `tbl_upload_preview` (
-  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `id` int(11) NOT NULL,
   `userId` int(11) NOT NULL,
   `upload_id` varchar(110) DEFAULT NULL,
   `file_name` varchar(255) DEFAULT NULL,
   `create_at` datetime DEFAULT NULL,
   `update_at` datetime DEFAULT NULL,
-  `path` varchar(255) DEFAULT NULL,
-  PRIMARY KEY (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8;
+  `path` varchar(255) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
--- ----------------------------
--- Records of tbl_upload_preview
--- ----------------------------
-INSERT INTO `tbl_upload_preview` VALUES ('1', '2', '5', 'sample-3pp.pdf', '2019-12-23 19:11:04', null, 'uploads/Preview/sample-3pp.pdf');
+--
+-- Dumping data for table `tbl_upload_preview`
+--
 
--- ----------------------------
--- Table structure for `tbl_user`
--- ----------------------------
-DROP TABLE IF EXISTS `tbl_user`;
+INSERT INTO `tbl_upload_preview` (`id`, `userId`, `upload_id`, `file_name`, `create_at`, `update_at`, `path`) VALUES
+(2, 2, '1', 'ใบเสนอราคาfurnitmall_31-10-2562_(2).pdf', '2019-12-24 10:27:17', '2019-12-24 11:27:36', 'uploads/Preview/ใบเสนอราคาfurnitmall_31-10-2562_(2).pdf');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `tbl_user`
+--
+
 CREATE TABLE `tbl_user` (
-  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `id` int(11) NOT NULL,
   `passport` varchar(255) DEFAULT NULL,
   `email` varchar(255) DEFAULT NULL,
   `phone` varchar(255) DEFAULT NULL,
   `username` varchar(255) DEFAULT NULL,
   `password` varchar(255) DEFAULT NULL,
-  `created_at` timestamp NULL DEFAULT NULL ON UPDATE current_timestamp(),
-  PRIMARY KEY (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8;
+  `created_at` timestamp NULL DEFAULT NULL ON UPDATE current_timestamp()
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
--- ----------------------------
--- Records of tbl_user
--- ----------------------------
-INSERT INTO `tbl_user` VALUES ('1', '1659900740516', 'famnoii2550@gmail.com', '0968138751', 'worapong srisawan', '94d2be2e13cce22e63f410e98d6d59a9', '2019-12-11 04:07:28');
-INSERT INTO `tbl_user` VALUES ('2', '1269900232221', 'jame0925623256@gmail.com', '0925623256', 'Nattaphon Kiattikul', 'e10adc3949ba59abbe56e057f20f883e', '2019-12-13 08:25:38');
+--
+-- Dumping data for table `tbl_user`
+--
+
+INSERT INTO `tbl_user` (`id`, `passport`, `email`, `phone`, `username`, `password`, `created_at`) VALUES
+(1, '1659900740516', 'famnoii2550@gmail.com', '0968138751', 'worapong srisawan', '94d2be2e13cce22e63f410e98d6d59a9', '2019-12-10 21:07:28'),
+(2, '1269900232221', 'jame0925623256@gmail.com', '0925623256', 'Nattaphon Kiattikul', 'e10adc3949ba59abbe56e057f20f883e', '2019-12-13 01:25:38'),
+(3, '3951500', 'pat.088english@gmai.com', '0652074798', 'Wuttipat', 'df20d70924b67d115855be1c68506dfa', '2019-12-20 01:03:57'),
+(5, '1898697', 'pat.088english@gmail.com', '0652074798', 'rat', 'bd030b8837aaa6f62a9d96d8836e79c0', '2019-12-20 05:23:40');
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `tbl_follow`
+--
+ALTER TABLE `tbl_follow`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `tbl_paypal`
+--
+ALTER TABLE `tbl_paypal`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `tbl_upload`
+--
+ALTER TABLE `tbl_upload`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `tbl_upload_full`
+--
+ALTER TABLE `tbl_upload_full`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `tbl_upload_preview`
+--
+ALTER TABLE `tbl_upload_preview`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `tbl_user`
+--
+ALTER TABLE `tbl_user`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `tbl_follow`
+--
+ALTER TABLE `tbl_follow`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `tbl_paypal`
+--
+ALTER TABLE `tbl_paypal`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+
+--
+-- AUTO_INCREMENT for table `tbl_upload`
+--
+ALTER TABLE `tbl_upload`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+
+--
+-- AUTO_INCREMENT for table `tbl_upload_full`
+--
+ALTER TABLE `tbl_upload_full`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `tbl_upload_preview`
+--
+ALTER TABLE `tbl_upload_preview`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+
+--
+-- AUTO_INCREMENT for table `tbl_user`
+--
+ALTER TABLE `tbl_user`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
