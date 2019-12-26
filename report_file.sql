e24f533d66164e50024277ec0e86833e75ba94f3
Nattaphon Kiattikul <jame0925623256@gmail.com>
2019-12-26 18:56:14 +0700

    ลบ db และ เอกสาร

--- report_file.sql
+++ report_file.sql
@@ -10,7 +10,7 @@ Target Server Type    : MYSQL
 Target Server Version : 50505
 File Encoding         : 65001
 
-Date: 2019-12-25 18:59:23
+Date: 2019-12-26 18:55:50
 */
 
 SET FOREIGN_KEY_CHECKS=0;
@@ -46,12 +46,11 @@ CREATE TABLE `tbl_paypal` (
   `last_name` varchar(150) NOT NULL,
   `create_time` datetime NOT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 -- ----------------------------
 -- Records of tbl_paypal
 -- ----------------------------
-INSERT INTO `tbl_paypal` VALUES ('1', '5XG65837WK7507415', 'J3GJEC9GX23VS', '1', 'USD', '3', 'John', 'Doe', '2019-10-01 11:50:00');
 
 -- ----------------------------
 -- Table structure for `tbl_select_item`
@@ -63,20 +62,30 @@ CREATE TABLE `tbl_select_item` (
   `created_at` datetime DEFAULT NULL,
   `updated_at` datetime DEFAULT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=10 DEFAULT CHARSET=utf8;
+) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8;
 
 -- ----------------------------
 -- Records of tbl_select_item
 -- ----------------------------
-INSERT INTO `tbl_select_item` VALUES ('1', 'หนังสือ', '2019-12-25 15:25:32', null);
-INSERT INTO `tbl_select_item` VALUES ('2', 'เกม', '2019-12-25 15:25:40', null);
-INSERT INTO `tbl_select_item` VALUES ('3', 'ภาพยนต์', '2019-12-25 15:25:43', null);
-INSERT INTO `tbl_select_item` VALUES ('4', 'วรรณกรรม', '2019-12-25 15:25:45', null);
-INSERT INTO `tbl_select_item` VALUES ('5', 'ประวัติศาสตร์', '2019-12-25 15:25:46', null);
-INSERT INTO `tbl_select_item` VALUES ('6', 'คณิตศาสตร์', '2019-12-25 18:34:06', null);
-INSERT INTO `tbl_select_item` VALUES ('7', 'ดาราศาสตร์', '2019-12-25 18:37:03', null);
-INSERT INTO `tbl_select_item` VALUES ('8', 'ภาษาไทย', '2019-12-25 18:41:06', null);
-INSERT INTO `tbl_select_item` VALUES ('9', 'อาหาร', '2019-12-25 18:43:11', null);
+INSERT INTO `tbl_select_item` VALUES ('1', 'อาหาร', '2019-12-26 13:31:33', null);
+INSERT INTO `tbl_select_item` VALUES ('2', 'คณิตศาสตร์', '2019-12-26 18:02:22', null);
+
+-- ----------------------------
+-- Table structure for `tbl_unlocks`
+-- ----------------------------
+DROP TABLE IF EXISTS `tbl_unlocks`;
+CREATE TABLE `tbl_unlocks` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `userId` int(11) DEFAULT NULL,
+  `upload_id` int(11) DEFAULT NULL,
+  `created_at` datetime DEFAULT NULL,
+  `updated_at` datetime DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- ----------------------------
+-- Records of tbl_unlocks
+-- ----------------------------
 
 -- ----------------------------
 -- Table structure for `tbl_upload`
@@ -93,15 +102,11 @@ CREATE TABLE `tbl_upload` (
   `create_at` datetime DEFAULT NULL,
   `update_at` datetime DEFAULT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=5 DEFAULT CHARSET=utf8;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 -- ----------------------------
 -- Records of tbl_upload
 -- ----------------------------
-INSERT INTO `tbl_upload` VALUES ('1', '2', '0', 'ปัญญาประดิษฐ์ (AI : Artificial Intelligence) คืออะไร ???', 'Electronic', 'MX001', 'เครื่องจักร(machine) ที่มีฟังก์ชันทีมีความสามารถในการทำความเข้าใจ เรียนรู้องค์ความรู้ต่างๆ อาทิเช่น การรับรู้  การเรียนรู้ การให้เหตุผล และการแก้ปัญหาต่างๆ  เครื่องจักรที่มีความสามารถเหล่านี้ก็ถือว่าเป็น ปัญญาประดิษฐ์  (AI : Artificial Intelligence) นั่นเอง\r\n\r\nเพราะฉะนั้นจึงสามารถกล่าวได้ว่า AI ถือกำเนิดขึ้นเมื่อเครื่องจักรมีความสามารถที่จะเรียนรู้นั่นเอง  ซึ่ง AI ก็ถูกแบ่งออกเป็นหลายระดับตามความสามารถหรือความฉลาด  โดยจะวัดจากความสามารถในการ ให้เหตุผล การพูด และทัศนคติของ AI ตัวนั้นๆ เมื่อเปรียบเทียบกับมนุษย์อย่างเราๆ', '2019-12-24 10:21:54', null);
-INSERT INTO `tbl_upload` VALUES ('2', '2', '3', 'เอกสารสำคัญ - โปรแกรม ร้าน ค้า ปลีก', 'ภาพยนต์', 'MX0032', 'โปรแกรมร้านค้าปลีก ค้าส่งราคา 2000 บาท ใช้งานง่าย บริการหลังการขายฟรี มีครบทั้ง ระบบขายสินค้า สต๊อกสินค้า บัญชี บาร์โค้ด รายงานยอดขาย ฯลฯ เอกสารสำคัญ ใบเสนอราคา ...', '2019-12-25 11:26:16', '2019-12-25 17:10:25');
-INSERT INTO `tbl_upload` VALUES ('3', '2', '1', 'Can I have two jquery onclick events in one element? - Stack Overflow', 'หนังสือ', 'MX008', '20 มิ.ย. 2556 - 3 คำตอบ - I know this has been asked before but I can\'t quite get the syntax of how to add my particular functions in one onclick even. Current onclick code', '2019-12-25 15:41:57', null);
-INSERT INTO `tbl_upload` VALUES ('4', '2', '9', 'อาหารไทย', 'อาหาร', 'อาหารไทย', 'อาหารไทย', '2019-12-25 18:47:00', null);
 
 -- ----------------------------
 -- Table structure for `tbl_upload_full`
@@ -116,12 +121,11 @@ CREATE TABLE `tbl_upload_full` (
   `update_at` datetime DEFAULT NULL,
   `path` varchar(255) DEFAULT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 -- ----------------------------
 -- Records of tbl_upload_full
 -- ----------------------------
-INSERT INTO `tbl_upload_full` VALUES ('1', '2', '2', 'ใบเสนอราคาfurnitmall_31-10-2562_(2).pdf', '2019-12-25 11:26:26', '2019-12-25 12:02:19', 'uploads/full/ใบเสนอราคาfurnitmall_31-10-2562_(2).pdf');
 
 -- ----------------------------
 -- Table structure for `tbl_upload_preview`
@@ -136,12 +140,11 @@ CREATE TABLE `tbl_upload_preview` (
   `update_at` datetime DEFAULT NULL,
   `path` varchar(255) DEFAULT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=13 DEFAULT CHARSET=utf8;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 -- ----------------------------
 -- Records of tbl_upload_preview
 -- ----------------------------
-INSERT INTO `tbl_upload_preview` VALUES ('4', '2', '2', 'หน้าเว็บของแอดมิน.pdf', '2019-12-25 11:26:22', '2019-12-25 12:02:52', 'uploads/Preview/หน้าเว็บของแอดมิน.pdf');
 
 -- ----------------------------
 -- Table structure for `tbl_user`
