9be408a78b270d8a4c3a02c03c3d97e3d3bbe8f3
miki <mikiboy004>
2020-01-08 11:20:59 +0700

    ประวัติ -7 วัน

--- application/modules/front-end/views/recently_accessed.php
+++ application/modules/front-end/views/recently_accessed.php
@@ -8,80 +8,25 @@
         <div class="col-lg-12 col-md-12 col-12 ">
             <article class="single_upload input-upload">
                 <div class="row">
+                <?php foreach ($recently as $key => $recently) { ?>
+                    <?php $DateT = date('Y-m-d')  ?>
+                   <?php  $produm = date('Y-m-d', strtotime('+7 day'. '+'.$recently['created_at']));?>
+                   <?php if($DateT >= $produm ) :?>
+                       
+                    <?php else :?>
                     <div class="col-3 text-right margin-5 product">
 
-                        <iframe src="public/image/ExportServlet.pdf" width="100%" height="620px" style="position:relative"></iframe>
-                        
+                        <iframe src="<?php echo $recently['path']?>" width="100%" height="450px" style="position:relative"></iframe>
+                        <?php  $Date = explode(" ",$recently['created_at']) ?>
                         <div style="color: #a7a7a7; display:flex;justify-content:space-around;">
-                            <span ><i class="fa fa-clock-o" aria-hidden="true"> 20/11/2562</i></span>
+                            <span ><i class="fa fa-clock-o" aria-hidden="true"><?php echo $Date[0] ?></i></span>
                             <span><i class="fa fa-unlock-alt" aria-hidden="true"> 5,000</i></span>
                             <span><i class="fa fa-eye" aria-hidden="true"> 1,501</i></span>
                         </div>
                     </div>
-                    <div class="col-3 text-right margin-5">
-                      
-                        <iframe src="public/image/ExportServlet.pdf" width="100%" height="620px"></iframe>
-                        <div style="color: #a7a7a7; display:flex;justify-content:space-around;">
-                            <span ><i class="fa fa-clock-o" aria-hidden="true"> 20/11/2562</i></span>
-                            <span><i class="fa fa-unlock-alt" aria-hidden="true"> 5,000</i></span>
-                            <span><i class="fa fa-eye" aria-hidden="true"> 1,501</i></span>
-                        </div>
-                    </div>
-                    <div class="col-3 text-right margin-5">
-                      
-                        <iframe src="public/image/ExportServlet.pdf" width="100%" height="620px"></iframe>
-                        <div style="color: #a7a7a7; display:flex;justify-content:space-around;">
-                            <span ><i class="fa fa-clock-o" aria-hidden="true"> 20/11/2562</i></span>
-                            <span><i class="fa fa-unlock-alt" aria-hidden="true"> 5,000</i></span>
-                            <span><i class="fa fa-eye" aria-hidden="true"> 1,501</i></span>
-                        </div>
-                    </div>
-                    <div class="col-3 text-right margin-5">
-                       
-                        <iframe src="public/image/ExportServlet.pdf" width="100%" height="620px"></iframe>
-                        <div  style="color: #a7a7a7; display:flex;justify-content:space-around;">
-                            <span><i class="fa fa-clock-o" aria-hidden="true"> 20/11/2562</i></span>
-                            <span><i class="fa fa-unlock-alt" aria-hidden="true"> 5,000</i></span>
-                            <span><i class="fa fa-eye" aria-hidden="true"> 1,501</i></span>
-                        </div>
-                    </div>
+                    <?php endif; ?>
+                <?php } ?>
 
-                    <div class="col-3 text-right margin-5">
-                       
-                        <iframe src="public/image/ExportServlet.pdf" width="100%" height="620px"></iframe>
-                        <div style="color: #a7a7a7; display:flex;justify-content:space-around;">
-                            <span ><i class="fa fa-clock-o" aria-hidden="true"> 20/11/2562</i></span>
-                            <span><i class="fa fa-unlock-alt" aria-hidden="true"> 5,000</i></span>
-                            <span><i class="fa fa-eye" aria-hidden="true"> 1,501</i></span>
-                        </div>
-                    </div>
-                    <div class="col-3 text-right margin-5">
-                      
-                        <iframe src="public/image/ExportServlet.pdf" width="100%" height="620px"></iframe>
-                        <div style="color: #a7a7a7; display:flex;justify-content:space-around;">
-                            <span ><i class="fa fa-clock-o" aria-hidden="true"> 20/11/2562</i></span>
-                            <span><i class="fa fa-unlock-alt" aria-hidden="true"> 5,000</i></span>
-                            <span><i class="fa fa-eye" aria-hidden="true"> 1,501</i></span>
-                        </div>
-                    </div>
-                    <div class="col-3 text-right margin-5">
-                       
-                        <iframe src="public/image/ExportServlet.pdf" width="100%" height="620px"></iframe>
-                        <div style="color: #a7a7a7; display:flex;justify-content:space-around;">
-                            <span ><i class="fa fa-clock-o" aria-hidden="true"> 20/11/2562</i></span>
-                            <span><i class="fa fa-unlock-alt" aria-hidden="true"> 5,000</i></span>
-                            <span><i class="fa fa-eye" aria-hidden="true"> 1,501</i></span>
-                        </div>
-                    </div>
-                    <div class="col-3 text-right margin-5">
-                     
-                        <iframe src="public/image/ExportServlet.pdf" width="100%" height="620px"></iframe>
-                        <div style="color: #a7a7a7; display:flex;justify-content:space-around;">
-                            <span ><i class="fa fa-clock-o" aria-hidden="true"> 20/11/2562</i></span>
-                            <span><i class="fa fa-unlock-alt" aria-hidden="true"> 5,000</i></span>
-                            <span><i class="fa fa-eye" aria-hidden="true"> 1,501</i></span>
-                        </div>
-                    </div>
                 </div>
             </article>
         </div>
