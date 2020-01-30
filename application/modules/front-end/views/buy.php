62547547a901d3a11944c44baed6b5905f190017
Nattaphon Kiattikul <jame0925623256@gmail.com>
2020-01-30 10:28:27 +0700

    buy 30/01/2563

--- application/modules/front-end/views/buy.php
+++ application/modules/front-end/views/buy.php
@@ -9,14 +9,17 @@
                          <div class="col-12">
                              <div class="tab">
                                  <label for="" class="font-size-upload">You can buy document. </label>
-
                                  <form action="buy_upload" class="dropzone" id="fileupload">
                                      <div class="dz-message needsclick">
                                          Drop files here or click to upload.<br>
                                          <span class="note needsclick">(This is just a demo dropzone. Selected files are <strong>not</strong> actually uploaded.)</span>
                                          <input type="text" name="userId" value="<?php echo $userId['id']; ?>" hidden>
                                      </div>
+                                     <div>
+                                         <input type="date" name="date" id="" class="form-control">
+                                     </div>
                                  </form>
+                                 <input type="button" id='uploadfiles' value='Upload Files'>
                              </div>
                          </div>
                          <div class="col-12 text-right" style="padding-top: 10px;">
@@ -30,3 +33,16 @@
      </div>
  </div>
  <!--wishlist area end -->
+
+ <script type="text/javascript">
+     Dropzone.autoDiscover = false;
+
+     var myDropzone = new Dropzone(".dropzone", {
+         autoProcessQueue: false,
+         parallelUploads: 10 // Number of files process at a time (default 2)
+     });
+
+     $('#uploadfiles').click(function() {
+         myDropzone.processQueue();
+     });
+ </script>
