467ceea117ea746c6599798f4638ec8bedb06842
miki <mikiboy004>
2020-03-16 16:59:52 +0700

    if

--- application/modules/back-end/controllers/Complete_ctr.php
+++ application/modules/back-end/controllers/Complete_ctr.php
@@ -24,70 +24,7 @@ class Complete_ctr extends CI_Controller
         }
     }
 
-    public function sendEmail_delivery()
-    {
-        $id =$this->input->get('id');
-        $email =$this->input->get('email');
-        $user = $this->db->get_where('tbl_user', ['id' => $email])->row_array();
-        $order = $this->db->get_where('tbl_upload_order_team', ['order_id' => $id])->result_array();
-
-        $this->db->where('order_id',$id);
-        $this->db->update('tbl_upload_order',['update_at' => date('Y-m-d H:i:s'),'is_confirm' => 1,'notify_user' => 0]);
-        $subject = 'test ip-soft';
-
-        $message = '<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css">';
-        $message .= '<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>';
-        $message .= '<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.16.0/umd/popper.min.js"></script>';
-        $message .= '<script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.min.js"></script>';
-        $message .= '<body style="background: #eee;">';
-
-        $message .= '<div style="text-align:center; margin:15px 0; color:#000000; font-size:18px;">Hello World</div>';
-
-
-        $message .= '<div style="text-align:center; margin:15px 0; color:#000000; font-size:18px;">Order ID : ' . $order[0]['order_id'] . '</div>';
-        foreach ($order as $order) {
-
-        $message .= '<a href="http://ip-soft.co.th/ipsoft/'.$order['path'].'">'.$order['file_name'].'</a>';
-        $message .= '<br>';
-        }
-
-        //$message .= '<div style="text-align:center; margin:15px 0; color:#000000; font-size:18px;">Price : '.$upload_order[0]['price_file'].'</div>';
-        //$message .= '<div style="text-align:center; margin:15px 0; color:#000000; font-size:18px;">Discount : '.$discount.'%</div>';
-        //$message .= '<div style="text-align:center; margin:15px 0; color:#000000; font-size:18px;">Customer ID : CM'.$upload_order[0]['userId'].'</div>';
-        $message .= '<div>';
-        $message .= '<div style="text-align: center;width:40%; margin:15px auto; background:#0063d1; font-size:28px;">';
-        $message .= 'Reject';
-        $message .= '</div>';
-        $message .= '</div>';
-        $message .= '</body>';
-
-        //config email settings
-        $config['protocol'] = 'smtp';
-        $config['smtp_host'] = 'smtp.gmail.com';
-        $config['smtp_port'] = '2002';
-        $config['smtp_user'] = 'infinityp.soft@gmail.com';
-        $config['smtp_pass'] = 'P@Ssw0rd';  //sender's password
-        $config['mailtype'] = 'html';
-        $config['charset'] = 'utf-8';
-        $config['wordwrap'] = 'TRUE';
-        $config['smtp_crypto'] = 'tls';
-        $config['newline'] = "\r\n";
-
-        //$file_path = 'uploads/' . $file_name;
-        $this->load->library('email', $config);
-        $this->email->set_newline("\r\n");
-        $this->email->from('infinityp.soft@gmail.com');
-        $this->email->to('infinityp.soft@gmail.com');
-        $this->email->subject($subject);
-        $this->email->message($message);
-        $this->email->set_mailtype('html');
-
-        if ($this->email->send() == true) {
-            echo '1';
-        } else {
-            echo '2';
-        }
-    }
+   
 
     public function book_complete_add_com()
     {
@@ -188,11 +125,13 @@ class Complete_ctr extends CI_Controller
 
 
         $message .= '<div style="text-align:center; margin:15px 0; color:#000000; font-size:18px;">Order ID : ' . $id . '</div>';
+        if(!empty($order_id)){
         foreach ($order_id as $key => $order_id) {
             $order = $this->db->get_where('tbl_upload_store', ['id' => $order_id])->row_array();
             $message .= '<a href="http://ip-soft.co.th/ipsoft/'.$order['path'].'">'.$order['file_name'].'</a>';
             $message .= '<br>';
        }
+    }
 
        foreach ($order_team as $key => $order_team) {
            $orderT = $this->db->get_where('tbl_upload_order_team', ['id' => $order_team])->row_array();
