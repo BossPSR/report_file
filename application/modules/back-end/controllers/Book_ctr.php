06a28dad88a4062f838b29511a9bcbec75119867
Nattaphon Kiattikul <jame0925623256@gmail.com>
2020-04-16 20:23:14 +0700

    Derliery แก้ไข

--- application/modules/back-end/controllers/Book_ctr.php
+++ application/modules/back-end/controllers/Book_ctr.php
@@ -13,7 +13,7 @@ class Book_ctr extends CI_Controller
     public function index()
     {
         if ($this->session->userdata('email_admin') != '') {
-  
+
             $this->load->view('options/header');
             $this->load->view('add_bookmark');
             $this->load->view('options/footer');
@@ -25,11 +25,11 @@ class Book_ctr extends CI_Controller
     public function  bookmark_notpay()
     {
         if ($this->session->userdata('email_admin') != '') {
-  
+
             $data['bookmark_all_not'] = $this->Store_model->bookmark_all_not();
-            
+
             $this->load->view('options/header');
-            $this->load->view('bookmark_notpay',$data);
+            $this->load->view('bookmark_notpay', $data);
             $this->load->view('options/footer');
         } else {
             $this->load->view('login');
@@ -47,7 +47,7 @@ class Book_ctr extends CI_Controller
             'update_at'                        => date('Y-m-d H:i:s')
 
         );
-                       $this->db->where('order_id', $id_order);
+        $this->db->where('order_id', $id_order);
         $resultsedit = $this->db->update('tbl_upload_order', $data);
         $upload_order =  $this->db->get_where('tbl_upload_order', ['order_id' => $id_order])->result_array();
         $this->sendEmail($upload_order);
@@ -59,7 +59,7 @@ class Book_ctr extends CI_Controller
         }
         return redirect('Bookmark_notpay');
     }
-   
+
     private function sendEmail($upload_order)
     {
         $user = $this->db->get_where('tbl_user', ['idUser' => $upload_order[0]['userId']])->row_array();
@@ -89,19 +89,19 @@ class Book_ctr extends CI_Controller
 
         $subject = 'เอกสารการชำระเงิน จาก www.Report-file.com ';
 
-        
+
         $message  = '<body style="background: #eee;">';
-     
+
         $message .= '<div style="text-align:center; margin:15px 0; color:#000000; font-size:18px;"> เอกสารการชำระเงิน จาก www.Report-file.com  </div>';
         $message .= '<table align="center" style="font-size: 20px;border-collapse: separate!important; border-style: dotted;background: white;"  border="0">';
 
         $message .= '<tr>';
-            $message .= '<td rowspan="' . $numFile . '">';
-                $message .= ' File Name : ';
-            $message .= '</td>';
-            $message .= '<td>';
-                $message .= ' 1.' . $uploads[0] . ' ';
-            $message .= '</td>';
+        $message .= '<td rowspan="' . $numFile . '">';
+        $message .= ' File Name : ';
+        $message .= '</td>';
+        $message .= '<td>';
+        $message .= ' 1.' . $uploads[0] . ' ';
+        $message .= '</td>';
         $message .= '</tr>';
 
         $num_list = 1;
@@ -111,19 +111,19 @@ class Book_ctr extends CI_Controller
             }
             $num_list += 1;
             $message .= '<tr>';
-                $message .= '<td>';
-                $message .=  ' ' . $num_list . '.' . $upload . ' ';
-                $message .= '</td>';
+            $message .= '<td>';
+            $message .=  ' ' . $num_list . '.' . $upload . ' ';
+            $message .= '</td>';
             $message .= '</tr>';
         }
 
         $message .= '<tr>';
-            $message .= '<td>';
-                $message .= ' Order ID : ';
-            $message .= '</td>';
-            $message .= '<td>';
-                $message .= ' ' . $upload_order[0]['order_id'] . ' ';
-            $message .= '</td>';
+        $message .= '<td>';
+        $message .= ' Order ID : ';
+        $message .= '</td>';
+        $message .= '<td>';
+        $message .= ' ' . $upload_order[0]['order_id'] . ' ';
+        $message .= '</td>';
         $message .= '</tr>';
 
         $message .= '<tr>';
@@ -195,81 +195,70 @@ class Book_ctr extends CI_Controller
         } else {
             $this->session->set_flashdata('del_ss2', 'Not Successfully send Not Pay Edit information');
         }
-
     }
 
-    
+
     public function fileUpload_store_book()
     {
         // image_lib
         $DM = $this->input->post('DM');
 
-          $upload_book = $this->Store_model->bookmark_upload($DM);
+        $upload_book = $this->Store_model->bookmark_upload($DM);
 
-           $target_dir = "uploads/Store/"; // Upload directory
-            if (!empty($_FILES['file']['name'])) {
+        $target_dir = "uploads/Store/"; // Upload directory
+        if (!empty($_FILES['file']['name'])) {
 
-                // Set preference
-                $config['upload_path']     = 'uploads/Store/';
-                // $config['allowed_types'] 	= 'jpg|jpeg|png|gif|pdf|docx|xlsx|pptx';
-                $config['allowed_types']   = '*';
-                $config['max_size']        = '99999'; // max_size in kb
-                $config['file_name']     = $_FILES['file']['name'];
+            // Set preference
+            $config['upload_path']     = 'uploads/Store/';
+            // $config['allowed_types'] 	= 'jpg|jpeg|png|gif|pdf|docx|xlsx|pptx';
+            $config['allowed_types']   = '*';
+            $config['max_size']        = '99999'; // max_size in kb
+            $config['file_name']     = $_FILES['file']['name'];
 
-                //Load upload library
-                $this->load->library('upload', $config);
-                $this->upload->initialize($config);
-                // File upload
-                if ($this->upload->do_upload('file')) {
-                    // Get data about the file
-                    $uploadData = $this->upload->data();
+            //Load upload library
+            $this->load->library('upload', $config);
+            $this->upload->initialize($config);
+            // File upload
+            if ($this->upload->do_upload('file')) {
+                // Get data about the file
+                $uploadData = $this->upload->data();
 
-                    $data = array(
-                        'store_id'      =>  $upload_book['store_id'],
-                        'file_name'     => $uploadData['file_name'],
-                        'status_main_search' => 1,
-                        'status_chack' => 1,
-                        'relive_status' => 1,
-                        'path'          => 'uploads/Store/' . $uploadData['file_name'],
-                        'status_check_drop' => 11,
-                        'create_at'     => date('Y-m-d H:i:s'),
-                    );
-                    $this->db->insert('tbl_upload_store', $data);
-                }
+                $data = array(
+                    'store_id'      =>  $upload_book['store_id'],
+                    'file_name'     => $uploadData['file_name'],
+                    'status_main_search' => 1,
+                    'status_chack' => 1,
+                    'relive_status' => 1,
+                    'path'          => 'uploads/Store/' . $uploadData['file_name'],
+                    'status_check_drop' => 11,
+                    'create_at'     => date('Y-m-d H:i:s'),
+                );
+                $this->db->insert('tbl_upload_store', $data);
             }
-        
+        }
     }
-   
+
 
     public function sendEmail_delivery_pay()
     {
-        
-        $order_id =$this->input->post('order_id'); 
-        $order_team =$this->input->post('order_team');
-        $id =$this->input->post('id');       
-        $dm_id =$this->input->post('dm_id');        
 
-      
+        $order_id = $this->input->post('order_id');
+        $order_team = $this->input->post('order_team');
+        $id = $this->input->post('id');
+        $dm_id = $this->input->post('dm_id');
 
-        $this->db->where('order_id',$id);
-        $this->db->update('tbl_upload_order',['update_at' => date('Y-m-d H:i:s'),'status_delivery' => 1 ,'notify_team' => 0 ,'notify_user' => 0]);
-        foreach ($dm_id as $key =>$dm_id) {
-         
-    
-        $this->db->where('id_doc',$dm_id);
-        $this->db->update('tbl_upload_main_search',['update_at' => date('Y-m-d H:i:s')]);
+        $this->db->where('order_id', $id);
+        $this->db->update('tbl_upload_order', ['update_at' => date('Y-m-d H:i:s'), 'status_delivery' => 1, 'notify_team' => 0, 'notify_user' => 0]);
+        foreach ($dm_id as $key => $dm_id) {
 
+            $this->db->where('id_doc', $dm_id);
+            $this->db->update('tbl_upload_main_search', ['update_at' => date('Y-m-d H:i:s')]);
 
-       
+        }
 
+        $user_email = $this->db->get_where('tbl_upload_order', ['order_id' => $id])->row_array();
+        $user_email_send  = $this->db->get_where('tbl_user', ['idUser' => $user_email['userId']])->row_array();
 
-
-        
-
-  }
-       $user_email = $this->db->get_where('tbl_upload_order',['order_id'=> $order_id])->row_array();
-       $user_email_send  = $this->db->get_where('tbl_user',['order_id'=> $user_email['userId']])->row_array();
-      
 
         $subject = 'test ip-soft';
 
@@ -285,17 +274,16 @@ class Book_ctr extends CI_Controller
         $message .= '<div style="text-align:center; margin:15px 0; color:#000000; font-size:18px;">Order ID : ' . $id . '</div>';
         foreach ($order_id as $key => $order_id) {
             $order = $this->db->get_where('tbl_upload_store', ['id' => $order_id])->row_array();
-            $message .= '<a href="http://ip-soft.co.th/ipsoft/'.$order['path'].'">'.$order['file_name'].'</a>';
+            $message .= '<a href="http://ip-soft.co.th/ipsoft/' . $order['path'] . '">' . $order['file_name'] . '</a>';
             $message .= '<br>';
-       }
+        }
 
-       foreach ($order_team as $key => $order_team) {
-           $orderT = $this->db->get_where('tbl_upload_order_team', ['id' => $order_team])->row_array();
-           $message .= '<a href="http://ip-soft.co.th/ipsoft/'.$orderT['path'].'">'.$orderT['file_name'].'</a>';
-           $message .= '<br>';
+        foreach ($order_team as $key => $order_team) {
+            $orderT = $this->db->get_where('tbl_upload_order_team', ['id' => $order_team])->row_array();
+            $message .= '<a href="http://ip-soft.co.th/ipsoft/' . $orderT['path'] . '">' . $orderT['file_name'] . '</a>';
+            $message .= '<br>';
+        }
 
-      }
-     
 
 
 
@@ -351,18 +339,18 @@ class Book_ctr extends CI_Controller
             'create_at'                        => date('Y-m-d H:i:s')
 
         );
-      
+
         if ($this->db->insert('tbl_upload_team', $data))
-        
 
 
-        $data2 = array(
 
-            'status_bookmark_upload_to_team'      => 1,
-            'update_at'                        => date('Y-m-d H:i:s')
+            $data2 = array(
 
-        );
-                       $this->db->where('order_id',$this->input->post('order_id'));
+                'status_bookmark_upload_to_team'      => 1,
+                'update_at'                        => date('Y-m-d H:i:s')
+
+            );
+        $this->db->where('order_id', $this->input->post('order_id'));
         $resultsedit = $this->db->update('tbl_upload_order', $data2);
 
         if ($resultsedit > 0) {
@@ -372,5 +360,4 @@ class Book_ctr extends CI_Controller
         }
         return redirect('Bookmark');
     }
-
 }
