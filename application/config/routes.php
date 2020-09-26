60f34804ee17e1c4e5a3fde76138955d69ccb616
BossPSR <boss3075030750@gmail.com>
2020-09-26 20:30:08 +0700

    fix

--- application/config/routes.php
+++ application/config/routes.php
@@ -382,3 +382,4 @@ $route['delete_dm_folder']                          = 'back-end/Book_ctr/delete_
 $route['download_all_file_satisfied']               = 'back-end/Customer_order_ctr/download_all_file_satisfied';
 $route['download_all_file_satisfied_gt']            = 'back-end/Customer_order_ctr/download_all_file_satisfied_gt';
 $route['rename_filename_for_sell']                  = 'back-end/Store_ctr/rename_filename_for_sell';
+$route['complete_status']                  			= 'back-end/Complete_ctr/complete_status';
