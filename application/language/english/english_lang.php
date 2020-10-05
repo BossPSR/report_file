f99fe3f9b04751ff0cdc9b6fc65cf5b763aa159a
khim <pusitapp14@gmail.com>
2020-10-05 17:32:13 +0700

    lang

--- application/language/english/english_lang.php
+++ application/language/english/english_lang.php
@@ -5,4 +5,120 @@ $lang['Highestscore'] = ' Highest score';
 $lang['Selectitem'] = ' Select/item';
 $lang['name']     =     'Name';
 $lang['Dashboard']     =     'Dashboard';
-?>
+$lang['sign_up']     =     'Sign up';
+$lang['imaclient']     =     'I’m a client';
+$lang['imaTutor']     =     'I’m a Tutor';
+$lang['login']     =     'Log in';
+$lang['email']     =     'Email';
+$lang['password']     =     'Password';
+$lang['lostyourpassword']     =     'Forgot password';
+$lang['logout']     =     'Log out';
+$lang['welcometoTutorlearner']     =     'Welcome to Tutorlearner.com. We thank you for choosing us and putting your trust in our services';
+$lang['home']     =     'Home';
+$lang['myrewards']     =     'My Refund';
+$lang['mycommission']     =     'My commission';
+$lang['discount']     =     'Discounts';
+$lang['score']     =     'Point';
+$lang['mywallet']     =     'My wallet';
+$lang['deposit']     =     'Deposit';
+$lang['withdraw']     =     'Withdraw';
+$lang['myslip']     =     'My Receipt';
+$lang['paid']     =     'Paid';
+$lang['notpay']     =     'Not Pay';
+$lang['withdrawhistory']     =     'Withdraw History';
+$lang['package']     =     'Package';
+$lang['mysell']     =     'Earn Points';
+$lang['poster']     =     '[At 100 Points, You Get a 10% discount](You can use 1 discount per order)';
+$lang['checkdocument']     =     'Check that all files are for one subject only';
+$lang['dropfiles_01']     =     'Maximum file size: 500MB, maximum number of files: 30';
+$lang['uploadfiles']     =     'Upload';
+$lang['mybuy']     =     'Order Now';
+$lang['uploaddocument']     =     'Upload Assessment Instruction/Question file here';
+$lang['uploadguidelines']     =     'Upload guidelines, templates and other resources here (If Available)';
+$lang['mainfile']     =     'Maximum file size: 20MB, maximum number of files: 10';
+$lang['gtfile']     =     'Maximum file size: 50MB, maximum number of files: 20';
+$lang['choosethedate']     =     'Choose the due date to pick up your order';
+$lang['moredetails']     =     'More details';
+$lang['uploadfileorder']     =     'Upload Order';
+$lang['paymentlink']     =     'Payment through your payment link must be made within 24 hours.(Please contact us for late payments) ';
+$lang['myorder']     =     'My Orders';
+$lang['status']     =     'Status';
+$lang['order']     =     'Order ID';
+$lang['price']     =     'Price';
+$lang['maindocument']     =     'Assessment Instructions/Questions';
+$lang['gtdocument']     =     'Resources';
+$lang['teamfile']     =     'File Submissions';
+$lang['orderdate']     =     'Order Date';
+$lang['daterequired']     =     'Due date';
+$lang['tool']     =     'Tools';
+$lang['approved']     =     'Satisfactory';
+$lang['notapproved']     =     'Unsatisfactory';
+$lang['popup']     =     'Satisfaction Survey';
+$lang['dropfiles_02']     =     'Maximum file size: 50MB, maximum number of files: 20';
+$lang['uploading']     =     'This is only for uploading additional information for your current order. If you wish to make a new order please select the ‘Order Now’ function';
+$lang['feedback']     =     'Feedback';
+$lang['myselling']     =     'Point information';
+$lang['sale']     =     'Accepted';
+$lang['rejected']     =     'Denied';
+$lang['deduct']     =     'Deducted';
+$lang['acceptablepolicy']     =     'Acceptable Use Policy';
+$lang['termsofservice']     =     'Terms of Service';
+$lang['privacypolicy']     =     'Privacy Policy';
+$lang['revision_refundpolicy']     =     'Revision & Refund Policy';
+$lang['proposalfororder']     =     'Proposal for Order';
+
+$lang['wording']     =     'You can earn discounts easily by clicking the ‘Earn Point’ 
+function and uploading information to our website. These discounts can be used at any time and do not expire
+  Earn commissions of up to $40 by referring a friend
+ Earn discounts easily by clicking   the ‘Earn Point’';
+
+$lang['featuredetails']     =     '  Earn commissions of up to $40 by referring a friend
+ 100% safe and reliable wallet system 
+ Wallet system includes automatic withdrawals of your balance if you do not make an order
+Expert writers that have passed our detailed screening process
+ Adjustment warranty of up to 60 days
+ Earn discounts with no expiration date
+ Best prices for high quality services';
+
+
+$lang['personal_tm']     =     'Enter authentic and right personal information  ';
+$lang['signup_tm']     =     'Sign up';
+$lang['imaclient_tm']     =     'I’m a client';
+$lang['imatutor_tm']     =     'I’m a Tutor';
+$lang['login_tm']     =     'Log in';
+$lang['email_tm']     =     'Email';
+$lang['password_tm']     =     'Password';
+$lang['lostyourpass_tm']     =     'Forgot password';
+$lang['logout_tm']     =     'Log out';
+$lang['welcome_tm']     =     'Welcome to Filebackhelp.com. Thank you for being a part of our Tutor.';
+$lang['home_tm']     =     'Home';
+$lang['myrewards_tm']     =     'My Rewards';
+$lang['teammanual_tm']     =     'Manual';
+$lang['myjob_tm']     =     'My orders';
+$lang['myincome_tm']     =     'My Income';
+$lang['withdraw_tm']     =     'Withdraw';
+$lang['myslip_tm']     =     'My Receipt';
+$lang['orderlist_tm']     =     'My Order List';
+$lang['mytask_tm']     =     'My Assignment';
+$lang['daterequired_tm']     =     'Time remaining';
+$lang['idorder_tm']     =     'Order ID';
+$lang['maindocument_tm']     =     'Assessment Instructions/Questions';
+$lang['gtdocument_tm']     =     'Resources';
+$lang['dmdoc_tm']     =     'Answer Guides';
+$lang['teamfile_tm']     =     'File Submissions';
+$lang['wage_tm']     =     'Wage';
+$lang['selectitem01_tm']     =     'Subject';
+$lang['dateconfirm_tm']     =     'Date confirm';
+$lang['status_tm']     =     'Status';
+$lang['option_tm']     =     'Cancel Order';
+$lang['delivery_tm']     =     'Submit Here';
+$lang['youcanbuydocument_tm']     =     ' Before submitting your order, review all the information to make sure you have followed all the instructions and completed every question and  that the format of your information meets the instructions. (font, font size, layout etc.) 
+ Separate each task in an individual folder and specify which question is being answered in each template.';
+$lang['dropfiles_tm']     =     'Maximum file size: 500MB, maximum number of files: 50';
+$lang['chooseorder_tm']     =     'Choose the Order ID to submit the document';
+$lang['selectitem02_tm']     =     'Order ID';
+$lang['satisfactionsurvey_tm']     =     'Satisfaction Survey';
+$lang['myhistory_tm']     =     'Deductions';
+$lang['score_tm']     =     'Points';
+$lang['income_tm']     =     'My Income';
+
